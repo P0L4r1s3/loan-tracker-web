4e297ecbd6adf1e220bfb455075d765ba11b88fe
Julia27MH <mishaujulia@gmail.com>
2026-02-24 16:13:20 -0600

    Cambio a la tabla role_to_user
    -Cambiar de string a enum.

--- database/migrations/2026_02_12_222845_add_role_to_users.php
+++ database/migrations/2026_02_12_222845_add_role_to_users.php
@@ -6,25 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default('user');
+            $table->enum('role', ['admin', 'user'])
+                  ->default('user')
+                  ->change(); // 👈 importante
         });
-
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
-{
-    Schema::table('users', function (Blueprint $table) {
-        $table->dropColumn('role');
-    });
-}
-
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')
+                  ->default('user')
+                  ->change();
+        });
+    }
 };
